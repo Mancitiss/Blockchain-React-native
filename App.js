9bcbc1db2aa5b31416a4c4a53aa7493e138d61e8
Mancitiss <vovuongthanhtu@gmail.com>
2023-04-18 14:15:55 +0700

    first test

--- App.js
+++ App.js
@@ -1,7 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Peer } from "peerjs";
 
 export default function App() {
+  const connectPeer = process.env.TRUSTED_ID || "thao-iuu";
+  console.log("connectPeer ", connectPeer);
+  const peer = new Peer("mancitiss");
+  peer.on("connection", (conn) => {
+    conn.on("data", (data) => {
+      // Will print 'hi!'
+      console.log(data);
+    });
+    conn.on("open", () => {
+      conn.send("hello!");
+    });
+  });
+
+  if (peer.id != connectPeer.id){
+    const conn = peer.connect(connectPeer);
+    conn.on("open", () => {
+      conn.send("hi!");
+    });
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
