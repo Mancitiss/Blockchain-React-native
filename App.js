0644157b792a8f076882895b71e6a5a4726d7ab3
Mancitiss <vovuongthanhtu@gmail.com>
2023-04-24 10:10:39 +0700

    crypto is here

--- App.js
+++ App.js
@@ -2,37 +2,41 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import React, { useEffect, useState } from 'react';
 import * as Crypto from 'expo-crypto';
-global.crypto = Crypto;
-import WebRTC from 'react-native-webrtc';
-import Peer from 'simple-peer';
-//import { mediaDevices, RTCPeerConnection, RTCIceCandidate, RTCSessionDescription, } from "react-native-webrtc";
+//import wrtc from 'wrtc';
+//import Peer from 'simple-peer';
 
 export default function App() {
-  useEffect(() => {
-    var peer1 = new Peer({ initiator: true, wrtc: WebRTC })
-    var peer2 = new Peer({wrtc: WebRTC })
+  if (Crypto && Crypto.getRandomValues){
+    console.log("Crypto is defined");
+  }
+  // useEffect(() => {
+  //   console.log("hi");
+  //   var peer1 = new Peer({ initiator: true, wrtc: wrtc })
+  //   console.log("hello");
+  //   var peer2 = new Peer({wrtc: wrtc})
+  //   console.log("hello2");
 
-    peer1.on('signal', data => {
-      // when peer1 has signaling data, give it to peer2 somehow
-      peer2.signal(data)
-    })
+  //   peer1.on('signal', data => {
+  //     // when peer1 has signaling data, give it to peer2 somehow
+  //     peer2.signal(data)
+  //   })
 
-    peer2.on('signal', data => {
-      // when peer2 has signaling data, give it to peer1 somehow
-      peer1.signal(data)
-    })
+  //   peer2.on('signal', data => {
+  //     // when peer2 has signaling data, give it to peer1 somehow
+  //     peer1.signal(data)
+  //   })
 
-    peer1.on('connect', () => {
-      // wait for 'connect' event before using the data channel
-      peer1.send('hey peer2, how is it going?')
-    })
+  //   peer1.on('connect', () => {
+  //     // wait for 'connect' event before using the data channel
+  //     peer1.send('hey peer2, how is it going?')
+  //   })
 
-    peer2.on('data', data => {
-      // got a data channel message
-      console.log('got a message from peer1: ' + data)
-    })
+  //   peer2.on('data', data => {
+  //     // got a data channel message
+  //     console.log('got a message from peer1: ' + data)
+  //   })
 
-  }, []);
+  // }, []);
 
   return (
     <View style={styles.container}>
